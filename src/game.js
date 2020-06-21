159ac148a479582c5e0b3a6d126d930f7d0851ee
rohith2801 <rohithkumarksheerasagar@gmail.com>
2020-06-22 02:46:35 +0530

    initial UI for game.

--- src/game.js
+++ src/game.js
@@ -1,10 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { Table, TableBody, TableRow } from "@material-ui/core";
+
 import { useQuery } from "./useQuery";
+import { CustomTableCell } from "./table-cell.styled";
 
 const Game = () => {
+    const [rowCount, setRowCount] = useState(3);
     console.dir(useQuery("id"));
 
-    return <div>Tihor..</div>;
+    const onTableClick = (event) => {
+        console.warn(event.target.cellIndex);
+        console.warn(event);
+    };
+
+    const renderCell = () => {
+        const returnValue = [];
+        for (let row = 0; row < rowCount; row++) {
+            for (let col = 0; col < rowCount; col++) {
+                console.warn(row, col);
+            }
+        }
+
+        return returnValue;
+    };
+
+    return (
+        <>
+            <Table>
+                <TableBody>
+                    <TableRow>
+                        <CustomTableCell
+                            onClick={onTableClick}
+                        ></CustomTableCell>
+                        <CustomTableCell
+                            onClick={onTableClick}
+                        ></CustomTableCell>
+                        <CustomTableCell
+                            onClick={onTableClick}
+                        ></CustomTableCell>
+                    </TableRow>
+
+                    <TableRow>
+                        <CustomTableCell
+                            onClick={onTableClick}
+                        ></CustomTableCell>
+                        <CustomTableCell
+                            onClick={onTableClick}
+                        ></CustomTableCell>
+                        <CustomTableCell
+                            onClick={onTableClick}
+                        ></CustomTableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </>
+    );
 };
 
 export default Game;
---
+++ src/table-cell.styled.js
@@ -0,0 +1,5 @@
+import { TableCell, styled } from "@material-ui/core";
+
+export const CustomTableCell = styled(TableCell)({
+    border: "1px solid black",
+});
